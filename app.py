f167ed3839f4f0f4e3512d03d3a155a54a22cd33
bossweirk2 <104666171+bossweirk2@users.noreply.github.com>
2025-10-26 16:34:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ remove_files(7)
 
 st.title("Reconocimiento Óptico de Caracteres")
 st.subheader("Elige la fuente de la imágen, esta puede venir de la cámara o cargando un archivo")
+st.image()
 
 cam_ = st.checkbox("Usar Cámara")
 
